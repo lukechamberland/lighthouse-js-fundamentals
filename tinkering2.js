6ab1e73351596f6273c34d8749def6fc1c14ab5d
Luke Chamberland <luke.chamberland@shaw.ca>
2023-01-12 21:26:38 +0000

    More work for my git repository

---
+++ tinkering2.js
@@ -0,0 +1,8 @@
+const myMoney = 50.00
+const cost = 35.00
+
+if (myMoney >= cost) {
+  console.log("purchase complete")
+} else {
+  console.log("sorry, not enough cash UwU")
+}
---
+++ whichschool.js
@@ -0,0 +1,11 @@
+let whichSchool = function(x); {
+
+  if (whichSchool < 13) {
+    console.log("Elementary School.");
+  } else if (whichSchool > 13 && whichSchool < 18) {
+    console.log("Secondary School.");
+  } else {
+    console.log("Lighthouse Labs.");
+ }
+}
+whichSchool(10)
